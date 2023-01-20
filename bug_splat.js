eef739fabee7e54afb5f43f72a63ee6ab0db82ab
David <davyu735@gmail.com>
2023-01-20 00:10:34 -0500

    feat: added new requirements

--- bug_splat.js
+++ bug_splat.js
@@ -1,81 +1,87 @@
-const images = ["images/rattata.png", "images/splat.png"]
+const images = ["images/rattata.png", "images/splat.png"];
 
-const defiantComments = ["Is that all you got?",
-                        "You suck!",
-                        "So weak",
-                        "You smell",
-                        "You should run away",
-                        "Sleep with one eye open",
-                        "You're done",
-                        "I'm yawning right now"
-                    ]
+const defiantComments = [
+  "Is that all you got?",
+  "You suck!",
+  "So weak",
+  "You smell",
+  "You should run away",
+  "Sleep with one eye open",
+  "You're done",
+  "I'm yawning right now",
+];
 
-const mercyComments = ["No please stop!", 
-                        "I have a family", 
-                        "Mercy!", 
-                        "I'll do what you want", 
-                        "Help me!",
-                        "You don't have to do this",
-                        "Wait! I'll buy you dinner"
-                    ]
+const mercyComments = [
+  "No please stop!",
+  "I have a family",
+  "Mercy!",
+  "I'll do what you want",
+  "Help me!",
+  "You don't have to do this",
+  "Wait! I'll buy you dinner",
+];
 
-const splattedRats = []
+const splattedRats = [];
 
-const usedDefiantComments = []
+const usedDefiantComments = [];
 
-const usedMercyComments = []
+const usedMercyComments = [];
 
 let numOfRats = 15;
 
 function initialize() {
-    document.getElementById("resetbutton").addEventListener("click", reset);
+  document.getElementById("resetbutton").addEventListener("click", reset);
 }
 
 let sayComment = (ratsLeft) => {
-    // if (ratsLeft === 0) {
-    //     document.getElementById("comment").innerHTML = "You've splatted us all.";
-    // }
-    (ratsLeft === 0) 
-        ? document.getElementById("comment").innerHTML = "You've splatted us all."
-        : console.log("There are still rats remaining"); 
-    if (ratsLeft >= 8) {
-        randomDefiantComment = Math.floor(Math.random() * (defiantComments.length));
-        document.getElementById("comment").innerHTML = `Comment: ${defiantComments[randomDefiantComment]}`;
-        usedDefiantComments.push(defiantComments[randomDefiantComment]);
-        defiantComments.splice(randomDefiantComment, 1);
-    } else if (ratsLeft < 8 && ratsLeft > 0) {
-        randomMercyComment = Math.floor(Math.random() * (mercyComments.length));
-        document.getElementById("comment").innerHTML = `Comment: ${mercyComments[randomMercyComment]}`;
-        usedMercyComments.push(mercyComments[randomMercyComment]);
-        mercyComments.splice(randomMercyComment, 1);
-    }
-}
+  ratsLeft === 0
+    ? (document.getElementById("comment").innerHTML = "You've splatted us all.")
+    : console.log("There are still rats remaining");
+  if (ratsLeft >= 8) {
+    randomDefiantComment = Math.floor(Math.random() * defiantComments.length);
+    document.getElementById(
+      "comment"
+    ).innerHTML = `Comment: ${defiantComments[randomDefiantComment]}`;
+    usedDefiantComments.push(defiantComments[randomDefiantComment]);
+    defiantComments.splice(randomDefiantComment, 1);
+  } else if (ratsLeft < 8 && ratsLeft > 0) {
+    randomMercyComment = Math.floor(Math.random() * mercyComments.length);
+    document.getElementById(
+      "comment"
+    ).innerHTML = `Comment: ${mercyComments[randomMercyComment]}`;
+    usedMercyComments.push(mercyComments[randomMercyComment]);
+    mercyComments.splice(randomMercyComment, 1);
+  }
+};
 
 let splat = (rat) => {
-    if (splattedRats.includes(rat) != true) {
-        document.getElementById(rat).src = "images/splat.png";
-        numOfRats--;
-        sayComment(numOfRats);
-        splattedRats.push(rat);
-    }
-}
+  if (splattedRats.includes(rat) != true) {
+    document.getElementById(rat).src = "images/splat.png";
+    numOfRats--;
+    sayComment(numOfRats);
+    splattedRats.push(rat);
+    let splatNoise = new Audio("images/splatSound.mp3");
+    splatNoise.play();
+  }
+};
 
 function reset() {
-    for (comment = 0; comment < usedDefiantComments.length; comment++) {
-        defiantComments.push(usedDefiantComments[comment]);
-    }
-    usedDefiantComments.length = 0;
-    for (comment = 0; comment < usedMercyComments.length; comment++) {
-        mercyComments.push(usedMercyComments[comment]);
-    }
-    usedMercyComments.length = 0;
-    numOfRats = 15;
+  for (comment = 0; comment < usedDefiantComments.length; comment++) {
+    defiantComments.push(usedDefiantComments[comment]);
+  }
+  usedDefiantComments.length = 0;
+  for (comment = 0; comment < usedMercyComments.length; comment++) {
+    mercyComments.push(usedMercyComments[comment]);
+  }
+  usedMercyComments.length = 0;
+  numOfRats = 15;
 
-    for (rat = 0; rat < splattedRats.length; rat++) {
-        document.getElementById(splattedRats[rat]).src = "images/rattata.png";
-    }
+  const imgElements = document.querySelectorAll("img");
+  for (let i = 0; i < imgElements.length; i++) {
+    imgElements[i].src = "images/rattata.png";
+  }
 
-    splattedRats.length = 0;
+  splattedRats.length = 0;
 
-    document.getElementById("comment").innerHTML = "";
+  document.getElementById("comment").innerHTML = "";
 }
